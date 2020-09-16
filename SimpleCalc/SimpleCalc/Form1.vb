507d8a5994751e18798cf94d9dc099a3e44f5081
Tsuyoshi-Nagata <69778135+Tsuyoshi-Nagata@users.noreply.github.com>
2020-09-16 14:54:55 +0900

    サブルーチンで入力データの型チェック機能修正

--- SimpleCalc/SimpleCalc/Form1.vb
+++ SimpleCalc/SimpleCalc/Form1.vb
@@ -1,23 +1,14 @@
 ﻿Public Class Form1
     Private Sub CalcButton_Click(sender As Object, e As EventArgs) Handles CalcButton.Click
+        '変数の宣言
         Dim valueLeft As Integer '入力値１用の整数型変数
         Dim valueRight As Integer '入力値２用の整数型変数
         Dim valueAnswer As Integer '計算結果用の整数型変数
 
 
         '値の取り込み
-        If Integer.TryParse(Input1TextBox.Text, valueLeft) = True Then
-            valueLeft = Integer.Parse(Input1TextBox.Text) '入力値１を整数型に変換後代入
-        Else
-            valueLeft = 0 '0を代入
-        End If
-
-        If Integer.TryParse(Input2TextBox.Text, valueRight) = True Then
-            valueRight = Integer.Parse(Input2TextBox.Text) '入力値２を整数型に変換後代入
-        Else
-            valueRight = 0 '0を代入
-        End If
-
+        valueLeft = InputCheckFunc(Input1TextBox.Text, valueLeft) '入力値１を整数型に変換後代入
+        valueRight = InputCheckFunc(Input2TextBox.Text, valueRight) '入力値２を整数型に変換後代入
         '取り込んだ値の計算
         valueAnswer = valueLeft + valueRight
 
@@ -26,4 +17,18 @@
 
 
     End Sub
+
+    '入力値をチェックするファンクション
+    Private Function InputCheckFunc(textValue As String, checkValue As Integer) As Integer
+        If Integer.TryParse(textValue, checkValue) = True Then
+            checkValue = Integer.Parse(textValue)
+        Else
+            checkValue = 0
+        End If
+
+        Return checkValue
+    End Function
+
+
+
 End Class
