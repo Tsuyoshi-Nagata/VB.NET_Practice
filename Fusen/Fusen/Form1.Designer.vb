7c89cef7fb8e8888d3bde18866bb436d1f2bd7c7
Tsuyoshi-Nagata <69778135+Tsuyoshi-Nagata@users.noreply.github.com>
2020-09-16 18:15:53 +0900

    付箋アプリ：新規追加

---
+++ Fusen/Fusen/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormFusen
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextFusenMemo = New System.Windows.Forms.TextBox()
+        Me.ColorDialogFusen = New System.Windows.Forms.ColorDialog()
+        Me.SuspendLayout()
+        '
+        'TextFusenMemo
+        '
+        Me.TextFusenMemo.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextFusenMemo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextFusenMemo.Location = New System.Drawing.Point(0, 0)
+        Me.TextFusenMemo.Multiline = True
+        Me.TextFusenMemo.Name = "TextFusenMemo"
+        Me.TextFusenMemo.Size = New System.Drawing.Size(800, 450)
+        Me.TextFusenMemo.TabIndex = 0
+        '
+        'FormFusen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextFusenMemo)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FormFusen"
+        Me.Opacity = 0.6R
+        Me.Text = "Form1"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextFusenMemo As TextBox
+    Friend WithEvents ColorDialogFusen As ColorDialog
+End Class
---
+++ Fusen/Fusen/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class FormFusen
+    'マウスのx座標
+    'マウスのy座標
+    Private MouseX As Integer
+    Private MouseY As Integer
+
+
+    'テキストボックスにキーボードから文字を入力したとき
+    Private Sub TextFusenMemo_KeyDown(sender As Object, e As KeyEventArgs) Handles TextFusenMemo.KeyDown
+        '＜判定＞押されたキーが「Esc」？
+        If (e.KeyCode = Keys.Escape) Then
+            '「Yes」なら
+            '   アプリ終了
+            Me.Close()
+        End If
+
+    End Sub
+
+    Private Sub TextFusenMemo_MouseDown(sender As Object, e As MouseEventArgs) Handles TextFusenMemo.MouseDown
+        '＜判定＞押されたボタンがマウスの左ボタン？
+        If (e.Button = MouseButtons.Left) Then
+            '「Yes」なら
+            '   マウスのx座標を記憶
+            '   マウスのy座標を記憶
+            Me.MouseX = e.X
+            Me.MouseY = e.Y
+        End If
+    End Sub
+
+    'テキストボックスでクリックしたマウスを移動させたとき
+    Private Sub TextFusenMemo_MouseMove(sender As Object, e As MouseEventArgs) Handles TextFusenMemo.MouseMove
+        '＜判定＞押されたボタンがマウスの左ボタン？
+        If (e.Button = MouseButtons.Left) Then
+            '「Yes」なら
+            '   フォームの横位置を更新
+            '   フォームの縦位置を更新
+            Me.Left = Me.Left + e.X - MouseX
+            Me.Top = Me.Top + e.Y - MouseY
+        End If
+    End Sub
+
+    'テキストボックスをマウスでダブルクリックしたとき
+    Private Sub TextFusenMemo_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles TextFusenMemo.MouseDoubleClick
+        '色の設定ダイアログを表示する
+        ColorDialogFusen.ShowDialog()
+
+        'テキストボックスの背景色を色の設定ダイアログで選んだ色に設定する
+        TextFusenMemo.BackColor = ColorDialogFusen.Color
+    End Sub
+End Class
