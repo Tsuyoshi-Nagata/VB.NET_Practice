4ca38edccab4a5ea91edbe53816744b439a3b507
Tsuyoshi-Nagata <69778135+Tsuyoshi-Nagata@users.noreply.github.com>
2020-09-17 18:42:41 +0900

    Gmail到着通知アプリ：新規追加

---
+++ GmailCheck/GmailCheck/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormGmailCheck
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtNewMail = New System.Windows.Forms.TextBox()
+        Me.butStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 60000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "メールアドレス"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 15)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "パスワード"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(118, 31)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(100, 22)
+        Me.txtAddress.TabIndex = 1
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(118, 65)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
+        Me.txtPassword.Size = New System.Drawing.Size(100, 22)
+        Me.txtPassword.TabIndex = 1
+        '
+        'txtNewMail
+        '
+        Me.txtNewMail.Location = New System.Drawing.Point(12, 144)
+        Me.txtNewMail.Multiline = True
+        Me.txtNewMail.Name = "txtNewMail"
+        Me.txtNewMail.Size = New System.Drawing.Size(258, 97)
+        Me.txtNewMail.TabIndex = 1
+        '
+        'butStart
+        '
+        Me.butStart.Location = New System.Drawing.Point(12, 106)
+        Me.butStart.Name = "butStart"
+        Me.butStart.Size = New System.Drawing.Size(258, 23)
+        Me.butStart.TabIndex = 2
+        Me.butStart.Text = "チェック開始"
+        Me.butStart.UseVisualStyleBackColor = True
+        '
+        'FormGmailCheck
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.butStart)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtNewMail)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormGmailCheck"
+        Me.Text = "Gmailチェッカー"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents txtNewMail As TextBox
+    Friend WithEvents butStart As Button
+End Class
---
+++ GmailCheck/GmailCheck/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class FormGmailCheck
+    Private count As Integer = -1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim client As New OpenPop.Pop3.Pop3Client()
+
+        client.Connect("pop.gmail.com", 995, True)
+        client.Authenticate(txtAddress.Text, txtPassword.Text)
+
+        Dim messageCount As Integer = client.GetMessageCount()
+        If (count = -1) Then
+            count = messageCount
+        End If
+
+        For i As Integer = messageCount To count + 1 Step -1
+            txtNewMail.Text += client.GetMessage(i).Headers.Subject + vbCrLf
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            NotifyIcon1.BalloonTipTitle = "メールが届きました"
+            NotifyIcon1.BalloonTipText = client.GetMessage(i).Headers.Subject
+            NotifyIcon1.ShowBalloonTip(3000)
+
+        Next i
+
+        count = messageCount
+
+    End Sub
+
+    Private Sub butStart_Click(sender As Object, e As EventArgs) Handles butStart.Click
+        Timer1.Enabled = True
+    End Sub
+
+
+End Class
