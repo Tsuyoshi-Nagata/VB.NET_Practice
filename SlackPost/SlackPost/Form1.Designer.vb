4ab806802536cc77c4e670fa67911d89a488078f
Tsuyoshi-Nagata <69778135+Tsuyoshi-Nagata@users.noreply.github.com>
2020-09-18 13:22:04 +0900

    Slack投稿アプリ：新規追加

---
+++ SlackPost/SlackPost/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormSlackPost
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.butPost = New System.Windows.Forms.Button()
+        Me.txtPost = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'butPost
+        '
+        Me.butPost.Location = New System.Drawing.Point(195, 216)
+        Me.butPost.Name = "butPost"
+        Me.butPost.Size = New System.Drawing.Size(75, 31)
+        Me.butPost.TabIndex = 0
+        Me.butPost.Text = "投稿"
+        Me.butPost.UseVisualStyleBackColor = True
+        '
+        'txtPost
+        '
+        Me.txtPost.Location = New System.Drawing.Point(12, 24)
+        Me.txtPost.Multiline = True
+        Me.txtPost.Name = "txtPost"
+        Me.txtPost.Size = New System.Drawing.Size(258, 186)
+        Me.txtPost.TabIndex = 1
+        '
+        'FormSlackPost
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 253)
+        Me.Controls.Add(Me.txtPost)
+        Me.Controls.Add(Me.butPost)
+        Me.Name = "FormSlackPost"
+        Me.Text = "Slack投稿"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents butPost As Button
+    Friend WithEvents txtPost As TextBox
+End Class
---
+++ SlackPost/SlackPost/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class FormSlackPost
+    Private Sub butPost_Click(sender As Object, e As EventArgs) Handles butPost.Click
+        Dim strWebHookurl As String = "https://hooks.slack.com/services/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"
+
+        Dim strData As String = String.Format("{{'text':'{0}'}},txtPost", txtPost.Text)
+
+        Dim client As System.Net.WebClient = New System.Net.WebClient()
+        client.Headers.Add(System.Net.HttpRequestHeader.ContentType,
+                           "application/json;charset=UTF-8")
+        client.Encoding = System.Text.Encoding.UTF8
+
+        Dim reply As String = client.UploadString(strWebHookurl, strData)
+
+        MessageBox.Show(reply)
+
+    End Sub
+End Class
